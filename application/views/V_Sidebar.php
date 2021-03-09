6d8880941076bb3dbf3cc9e9f0376e104bc58ae1
juanprasetyo <juanprasetyo040502@gmail.com>
2021-03-09 22:55:56 +0700

    ubah url menu di sidebar

--- application/views/V_Sidebar.php
+++ application/views/V_Sidebar.php
@@ -1,9 +1,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
-      <a href="index3.html" class="brand-link text-center">
-          <!-- <img src="<?= base_url('assets/'); ?>img/logo/logo-square-white1.svg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8; width: 35px; background-color: #ffffff;">
-          <span class=" brand-text font-weight-light">Yeagerist Egg</span> -->
+      <a href="<?= base_url('about'); ?>" class="brand-link text-center">
           <img src="<?= base_url('assets/'); ?>img/logo/logo-rectangle-white.svg" alt="Logo Yeagerist" style="height: 50px;">
       </a>
 
@@ -25,7 +23,7 @@
                   <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
                   <li class="nav-item">
-                      <a href="#" class="nav-link">
+                      <a href="<?= base_url('dashboard'); ?>" class="nav-link">
                           <i class="nav-icon fas fa-home"></i>
                           <p>
                               Home
@@ -33,7 +31,7 @@
                       </a>
                   </li>
                   <li class="nav-item">
-                      <a href="#" class="nav-link">
+                      <a href="<?= base_url('about'); ?>" class="nav-link">
                           <i class="nav-icon fas fa-question"></i>
                           <p>
                               About
